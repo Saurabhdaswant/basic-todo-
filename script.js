0647e0b7e436a598ac90755dc0bb2a49863eb938
saurabh daswant <77434364+Saurabhdaswant@users.noreply.github.com>
2021-06-05 09:14:20 +0530

    inital commit
    the inital markup is done

---
+++ script.js
@@ -0,0 +1,8 @@
+//take the input from user
+//make a form
+//there should be input feild and add btn after addeing it on the screen
+//it should have delete btn
+//add button for adding it to the list
+//store it in the local storage
+//show it on the screen
+//delete btn for deleting it from the local storage
