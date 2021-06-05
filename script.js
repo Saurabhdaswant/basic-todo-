f992e8517fbcb2b1a9448c80d820fe24fcdc23f8
saurabh daswant <77434364+Saurabhdaswant@users.noreply.github.com>
2021-06-05 13:07:51 +0530

    the logic of complete and delete

--- script.js
+++ script.js
@@ -5,25 +5,41 @@ const submit = document.querySelector(".submit");
 const task = document.querySelector(".task");
 const showTask = document.querySelector(".showTask");
 const body = document.body;
-console.log(userInput);
+const form = document.querySelector(".form");
 
 function createTask(e) {
   e.preventDefault();
-  const realTask = userInput.value;
 
-  const taskHtml = `<form class="completeForm">
-    <span class="task"> ${realTask} </span>
-    <input type="checkbox" class="checkbox">
-    <button class="deleteTask">X</button>
-  </form>`;
+  const realTask = userInput.value;
+  const taskHtml = `<span class="task">${realTask}</span>
+  <input type="checkbox" class="checkbox">
+  <button class="deleteTask">X</button>`;
   showTask.innerHTML = taskHtml;
+  form.reset();
+}
+
+//check btn for completion
+function completeTask(e) {
+  // console.log(e);
+  if (!e.target.matches(".checkbox")) return;
+  showTask.firstChild.classList.toggle("good");
+  console.log("hidden");
+}
+
+//delete btn for deleting it from the local storage
+function deleteTask(e) {
+  if (!e.target.matches(".deleteTask")) return;
+  showTask.innerHTML = "";
+  console.log("deleted");
 }
 
 //add button for adding it to the list
 submit.addEventListener("click", createTask);
+showTask.addEventListener("click", completeTask);
+showTask.addEventListener("click", deleteTask);
+// showTask.addEventListener("click", fuck);
 
 //store it in the local storage
 //show it on the screen
-//delete btn for deleting it from the local storage
 
 console.log("in the production baby");
