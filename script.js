1ea9565f765b699afec25a964fa1d237742484ac
saurabh daswant <77434364+Saurabhdaswant@users.noreply.github.com>
2021-06-05 10:15:16 +0530

    add task logic

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
 //take the input from user
-//make a form
-//there should be input feild and add btn after addeing it on the screen
-//it should have delete btn
+
+const userInput = document.querySelector(".userInput");
+const submit = document.querySelector(".submit");
+const task = document.querySelector(".task");
+const showTask = document.querySelector(".showTask");
+const body = document.body;
+console.log(userInput);
+
+function createTask(e) {
+  e.preventDefault();
+  const realTask = userInput.value;
+
+  const taskHtml = `<form class="completeForm">
+    <span class="task"> ${realTask} </span>
+    <input type="checkbox" class="checkbox">
+    <button class="deleteTask">X</button>
+  </form>`;
+  showTask.innerHTML = taskHtml;
+}
+
 //add button for adding it to the list
+submit.addEventListener("click", createTask);
+
 //store it in the local storage
 //show it on the screen
 //delete btn for deleting it from the local storage
+
+console.log("in the production baby");
